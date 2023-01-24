81c9c77af40f29ec6ce7a1cba457ca7fba40ce75
Rodrigo de Oliveira Carvalho Soares <rodrigocarvalhosoares@gmail.com>
2023-01-23 21:41:43 -0300

    Finalização frontend

--- src/components/OrderModal/index.tsx
+++ src/components/OrderModal/index.tsx
@@ -1,17 +1,23 @@
 import closeIcon from '../../assets/images/close-icon.svg';
 import { Order } from '../../types/Order';
+import { formatCurrency } from '../../utils/formatCurrency';
 
-import { ModalBody, OrderDetails, Overlay } from "./styles"
+import { ModalBody, OrderDetails, Overlay, Actions } from "./styles"
 
 interface OrderModalProps {
   visible: boolean;
   order: Order | null;
+  onClose: () => void;
 }
 
-export function OrderModal({ visible, order }: OrderModalProps) {
+export function OrderModal({ visible, order, onClose }: OrderModalProps) {
   if (!visible || !order){
     return null;
   }
+
+  const total = order.products.reduce((total, { product, quantity }) => {
+    return total + (product.price * quantity)
+  }, 0)
 
   return (
     <Overlay>
@@ -19,7 +25,7 @@ export function OrderModal({ visible, order }: OrderModalProps) {
         <header>
           <strong>Mesa {order.table}</strong>
 
-          <button type="button">
+          <button type="button" onClick={onClose}>
             <img src={closeIcon} alt="Ícone de fechar" />
           </button>
         </header>
@@ -44,8 +50,42 @@ export function OrderModal({ visible, order }: OrderModalProps) {
         <OrderDetails>
           <strong>Itens</strong>
 
+          <div className="order-items">
+            {order.products.map(({ _id, product, quantity }) => (
+              <div className="item">
+                <img
+                  src={`http://localhost:3001/uploads/${product.imagePath}`}
+                  alt={product.name}
+                  width="56"
+                  height="28.51"
+                />
 
+                <span className="quantity">{quantity}x</span>
+
+                <div className="product-details">
+                  <strong>{product.name}</strong>
+                  <span>{formatCurrency(product.price)}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <div className="total">
+            <span>Total</span>
+            <strong>R$ 120,00</strong>
+          </div>
         </OrderDetails>
+
+        <Actions>
+          <button type="button" className="primary">
+            <span>👨‍🍳</span>
+            <strong>Iniciar Produção</strong>
+          </button>
+
+          <button type="button" className="secondary">
+            <strong>Cancelar Pedido</strong>
+          </button>
+        </Actions>
       </ModalBody>
     </Overlay>
   )
